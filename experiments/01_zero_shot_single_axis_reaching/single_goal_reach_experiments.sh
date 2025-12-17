ce658e143f37d111e141651c6bb68c7d72ff4aa1
Lasse Sofus Moller Sorensen <s185927@comp-ws6211.compute.dtu.dk>
2025-12-17 21:57:28 +0100

    Updated zero-shot reaching consolidated figyure

--- experiments/01_zero_shot_single_axis_reaching/single_goal_reach_experiments.sh
+++ experiments/01_zero_shot_single_axis_reaching/single_goal_reach_experiments.sh
@@ -15,15 +15,15 @@ export MUJOCO_GL=egl
 # Go to project root
 cd /home/s185927/thesis/robohive/robohive/robohive
 
-OUT_DIR="/home/s185927/thesis/experiments/01_zero_shot_single_axis_reaching/left_cam"
-COMMON_ARGS="--render offscreen --out_dir ${OUT_DIR} --fixed_target --planning_steps 5 --save_distance_data --max_episodes 10 --action_transform swap_xy_negate_x --enable_vjepa_planning --save_planning_images --camera_name left_cam"
+OUT_DIR="/home/s185927/thesis/experiments/01_zero_shot_single_axis_reaching/left_cam_long_planning"
+COMMON_ARGS="--render offscreen --out_dir ${OUT_DIR} --fixed_target --planning_steps 10 --save_distance_data --max_episodes 1 --action_transform swap_xy_negate_x --enable_vjepa_planning --save_planning_images --camera_name left_cam"
 
 # Run sampling for x, y, z
-python utils/robo_samples.py ${COMMON_ARGS} --experiment_type x
-python utils/robo_samples.py ${COMMON_ARGS} --experiment_type y
+# python utils/robo_samples.py ${COMMON_ARGS} --experiment_type x
+# python utils/robo_samples.py ${COMMON_ARGS} --experiment_type y
 python utils/robo_samples.py ${COMMON_ARGS} --experiment_type z
 
 # Run distance analysis plots for x, y, z
-python -m utils.plot_distance_analysis --experiment_type x --out_dir ${OUT_DIR}
-python -m utils.plot_distance_analysis --experiment_type y --out_dir ${OUT_DIR}
+# python -m utils.plot_distance_analysis --experiment_type x --out_dir ${OUT_DIR}
+# python -m utils.plot_distance_analysis --experiment_type y --out_dir ${OUT_DIR}
 python -m utils.plot_distance_analysis --experiment_type z --out_dir ${OUT_DIR}
